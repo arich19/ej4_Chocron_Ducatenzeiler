4e213a913d82ac648befe88c6677e755165d9ffe
ARIEL OFIR CHOCRON <49628292@ort>
2026-04-30 08:55:05 -0300

    creacion del programa

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ej4_Chocron_Ducatenzeiler_Schatzyki.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
